e8e3b8450c27561b5ab0d26a8611f95fa9bd5dad
Eric Vonk <mcvenco@gmail.com>
2023-03-06 22:30:37 +0100

    Initialise project with webpack

---
+++ index.js
@@ -0,0 +1,29 @@
+import { createMachine } from 'xstate';
+
+const trafficLight = createMachine({
+  id: 'light',
+  initial: 'green',
+  states: {
+    green: {
+      on: {
+        TIMER: 'yellow'
+      }
+    },
+    yellow: {
+      on: {
+        TIMER: 'red'
+      }
+    },
+    red: {
+      on: {
+        TIMER: 'green'
+      }
+    }
+  }
+});
+
+const currentState = 'green';
+
+const nextState = trafficLight.transition(currentState, 'TIMER').value;
+
+// => 'yellow'
