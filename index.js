c5cd0a3ffdecb5628081bc6ede53247f2fa98b3c
Eric Vonk <mcvenco@gmail.com>
2023-03-08 10:38:51 +0100

    Add all the traffic light functionality.

--- index.js
+++ index.js
@@ -1,29 +1,79 @@
-import { createMachine } from 'xstate';
+import { createMachine, interpret } from 'xstate';
 
+// initialise traffic light, using 'after' to change states with interval
 const trafficLight = createMachine({
-  id: 'light',
-  initial: 'green',
+  id: 'traffic-light',
+  initial: 'defaultCycle',
   states: {
-    green: {
-      on: {
-        TIMER: 'yellow'
+    defaultCycle: {
+      on: {EMERGENCY: 'emergencyCycle'},
+      initial: 'green',
+      states: {
+        green: {
+          after: {
+            5000: {target: 'yellow'}
+          },
+        },
+        yellow: {
+          after: {
+            2000: {target: 'red'}
+          },
+        },
+        red: {
+          after: {
+            5000: {target: 'green'}
+          },
+        },
       }
     },
-    yellow: {
-      on: {
-        TIMER: 'red'
+    emergencyCycle: {
+      initial: 'emergency',
+      states: {
+        emergency: {
+          after: {
+            // go to red light after the ambulance has passed, just for safety :-)
+            5000: {target: '#traffic-light.defaultCycle.red'}
+          }
+        }
       }
     },
-    red: {
-      on: {
-        TIMER: 'green'
-      }
-    }
   }
 });
 
-const currentState = 'green';
+// use xstate interpreter to run automated Timeout function
+const service = interpret(trafficLight).onTransition((state) => {
+  // add 'active' class to current state light
+  if (state.value.defaultCycle){
+    document.querySelector('.light.' + state.value.defaultCycle).classList.add('active')
+  }
 
-const nextState = trafficLight.transition(currentState, 'TIMER').value;
+  // remove 'active' class from previous state light
+  // (except with initial state or coming from emergencyCycle)
+  if (state.history  && !state.history.value.emergencyCycle) {
+    document.querySelector('.light.' + state.history.value.defaultCycle).classList.remove('active')
+  }
+});
 
-// => 'yellow'
+service.start();
+
+
+// emergency button functionality
+const emergencyButton = document.querySelector('#emergencyButton');
+const yellowLight = document.querySelector('.light.yellow');
+const blinkenLichten = document.querySelector('.light.yellow.blinking');
+
+emergencyButton.addEventListener('click', () => {
+  // only run this code when yellow light is not already blinking
+  if(!blinkenLichten){
+    // go to emergencyCycle
+    service.send('EMERGENCY')
+
+    // add blinking class to yellow
+    yellowLight.classList.add('blinking');
+
+    // after 5 seconds, remove yellow blinking
+    setTimeout(() => {
+      yellowLight.classList.remove('blinking');
+    }, 5000);
+  }
+})
